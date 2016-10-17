63824af6765d4f85703e8537ba2b6a25db60a598
hpneo <hpneo@hotmail.com>
2016-10-17 01:27:36 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,74 @@
+var slides = document.querySelectorAll('.slide'),
+    slidesProgress = document.querySelector('.slide-progress'),
+    currentIndex = location.hash ? Number(location.hash.replace('#', '')) : 0;
+
+function buildSnippet(snippet) {
+  snippet.classList.add('cm-s-neo');
+
+  CodeMirror.runMode(snippet.textContent, snippet.lang, snippet);
+}
+
+function buildEvents() {
+  window.addEventListener('keyup', function(event) {
+    if (event.target.tagName === 'INPUT') {
+      return;
+    }
+
+    var index = location.hash ? Number(location.hash.replace('#', '')) : 0;
+
+    if (event.keyCode === 39) {
+      index++;
+    }
+    else if (event.keyCode === 37) {
+      index--;
+    }
+
+    if (index < 0) {
+      index = 0;
+    }
+    else if (index > (slides.length - 1)) {
+      index = slides.length - 1;
+    }
+
+    goToSlide(index);
+  });
+
+  window.addEventListener('popstate', function(event) {
+    var index = location.hash ? Number(location.hash.replace('#', '')) : 0;
+
+    goToSlide(index);
+  });
+}
+
+function goToSlide(index) {
+  if (!slides[index]) {
+    return;
+  }
+
+  Array.prototype.forEach.call(slides, function(slide) {
+    slide.classList.remove('active');
+  });
+
+
+  if (!slides[index].dataset.rendered) {
+    Array.prototype.forEach.call(slides[index].querySelectorAll('pre[lang]'), buildSnippet);
+  }
+
+  slides[index].classList.add('active');
+  slides[index].dataset.rendered = true;
+
+  currentIndex = index;
+  slidesProgress.value = index;
+  location.hash = '#' + currentIndex;
+}
+
+function startSlides() {
+  var index = location.hash ? Number(location.hash.replace('#', '')) : 0;
+
+  slidesProgress.max = slides.length - 1;
+  slidesProgress.value = index;
+
+  buildEvents();
+
+  goToSlide(index);
+}
---
+++ websockets_demo.js
@@ -0,0 +1,34 @@
+// var connection = new WebSocket('ws://localhost:2376/v1.24/containers/websockets/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1');
+var commands = document.querySelector('.websockets-demo-container-commands'),
+    input = document.querySelector('.websockets-demo-input');
+
+input.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    connection.send(event.target.value + '\n');
+  }
+});
+
+var connection = new WebSocket('ws://54.88.32.109:4000/v1.22/containers/websockets_demo/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1');
+
+connection.addEventListener('open', function() {
+  console.log('open');
+});
+
+connection.addEventListener('close', function() {
+  console.log('close');
+});
+
+connection.addEventListener('error', function() {
+  console.log('error');
+});
+
+connection.addEventListener('message', function(event) {
+  var newCommand = document.createElement('pre');
+  newCommand.className = 'cm-s-neo';
+  newCommand.textContent = event.data;
+
+  CodeMirror.runMode(newCommand.textContent, 'shell', newCommand);
+
+  commands.appendChild(newCommand);
+  input.value = '';
+});
